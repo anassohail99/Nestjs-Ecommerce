a64f32a2667687e5bcbf1957701a114b16a4ce80
anassohail99 <anas.sohailabs1@gmail.com>
2020-11-28 02:04:13 +0500

    User Types

---
+++ src/types/user.ts
@@ -0,0 +1,18 @@
+// import { Address } from "cluster";
+import { Document } from 'mongoose';
+
+interface Address {
+  addr1: string;
+  addr2: string;
+  city: string;
+  country: string;
+  zip: number;
+}
+
+export interface User extends Document {
+  name: string;
+  readonly password: string;
+  seller: boolean;
+  address: Address;
+  created: Date;
+}
