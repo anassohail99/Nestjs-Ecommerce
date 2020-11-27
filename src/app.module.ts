1c9c4728df327980d55d30023978a1f26164bf54
anassohail99 <anas.sohailabs1@gmail.com>
2020-11-28 01:45:03 +0500

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
+
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
